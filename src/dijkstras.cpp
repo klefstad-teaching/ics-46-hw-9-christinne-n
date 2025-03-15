91ee0c170b92859c228ad176ae8372a8e2d68d27
christinne-n <christln4@uci.edu>
2025-03-15 20:17:38 +0000

    commit

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -61,8 +61,6 @@ void print_path(const vector<int>& v, int total)
     }
     for(size_t i=0; i<v.size(); ++i)
     {
-        if (i>0) 
-            cout << " ";
         cout << v[i] << " ";
     }
     cout<<"\nTotal cost is " << total << "\n";
