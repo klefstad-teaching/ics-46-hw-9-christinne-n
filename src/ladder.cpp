7a3e8b8a79263a0ec046cce55f941dfe1e61a7dd
christinne-n <christln4@uci.edu>
2025-03-15 19:33:53 +0000

    commit

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -8,15 +8,19 @@ void error(string word1, string word2, string msg)
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d)
 {
     if(abs((int)str1.size() - (int)str2.size()) > d)
-        return 0;
+        return false;
     int diff = 0;
-    for(size_t i = 0; i<min(str1.size(),str2.size()); ++i)
+    for(size_t i = 0, j = 0; i < str1.size() && j < str2.size(); ++i, ++j)
     {
         if(str1[i] != str2[i])
         {
             diff++;
             if (diff > d)
-                return 0;
+                return false;
+            if (str1.size() > str2.size())
+                j++;
+            else
+                i++;
         }
     }
     return diff + abs((int)str1.size() - (int)str2.size()) <= d;
@@ -27,6 +31,8 @@ bool is_adjacent(const string& word1, const string& word2)
 }
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list)
 {
+    if(begin_word == end_word) //checks if they are the same
+        return {begin_word};
     queue<vector<string>> ladder_queue; //ladder_queue = vector queue of string
     ladder_queue.push({begin_word});
     set<string> visited; //visited = set of strings
@@ -60,7 +66,6 @@ void load_words(set<string> & word_list, const string& file_name)
     ifstream file(file_name);
     if (!file) {
         throw runtime_error("Can't open input file");
-        return;
     }
     string word;
     while(file >> word)
